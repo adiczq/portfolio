3e2d5e8f2f36f0f5528c91ac31099425c4c531b1
Adrian Lacheta <adiczq@gmail.com>
2024-02-14 21:37:56 +0100

    Update Works.js

--- src/components/Works/Works.js
+++ src/components/Works/Works.js
@@ -4,6 +4,7 @@ import Portfolio1 from "../../assets/portfolio-1.png";
 import Portfolio2 from "../../assets/portfolio-2.png";
 import Portfolio3 from "../../assets/portfolio-3.png";
 import Portfolio4 from "../../assets/portfolio-4.png";
+import Portfolio5 from "../../assets/portfolio-5.png";
 
 const Works = () => {
   return (
@@ -16,17 +17,47 @@ const Works = () => {
         Voluptatibus pariatur earum ipsa veniam.
       </span>
       <div className="worksImgs">
-        <a href="https://adiczq.github.io/Project-IceCream/">
+        {/* <div className="card"> </div> */}
+
+        <a
+          className="card"
+          href="https://adiczq.github.io/Project-IceCream/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src={Portfolio1} alt="Portfolio1" className="worksImg" />
         </a>
-        <a href="https://adiczq.github.io/WeatherApp/">
+        <a
+          className="card"
+          href="https://adiczq.github.io/WeatherApp/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src={Portfolio2} alt="Portfolio2" className="worksImg" />
         </a>
-        <a href="https://adiczq.github.io/team-project-filmoteka/">
+        <a
+          className="card"
+          href="https://adiczq.github.io/team-project-filmoteka/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src={Portfolio3} alt="Portfolio3" className="worksImg" />
         </a>
-        <a href="https://adiczq.github.io/diceGame/">
+        <a
+          className="card"
+          href="https://adiczq.github.io/diceGame/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src={Portfolio4} alt="Portfolio4" className="worksImg" />
+        </a>
+        <a
+          className="card"
+          href="https://adiczq.github.io/billSpliter/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={Portfolio5} alt="Portfolio5" className="worksImg" />
         </a>
       </div>
     </section>
