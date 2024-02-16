1c8158d8cf40a16f5d0497f5fbc4ae383b493d20
Adrian Lacheta <adiczq@gmail.com>
2024-02-17 00:06:58 +0100

    Create AboutMe.js

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./AboutMe.css";
+
+const AboutMe = () => {
+  return (
+    <id id="aboutMe">
+      <h2 className="aboutMeTitle">About me</h2>
+      <div className="aboutMeDesc">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam
+        dolorum sapiente nihil veritatis excepturi architecto asperiores
+        quaerat. Assumenda aut esse nulla autem? Temporibus quisquam quia
+        facilis error cum, dolorem eveniet!
+      </div>
+    </id>
+  );
+};
+
+export default AboutMe;
