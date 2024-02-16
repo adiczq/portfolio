693be4c98387b3fb804daa391c3c265c5a12e941
Adrian Lacheta <adiczq@gmail.com>
2024-02-17 00:06:56 +0100

    Update Gallery.js

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -17,16 +17,20 @@ const Gallery = () => {
       breakpoint: { max: 4000, min: 3000 },
       items: 5,
     },
+    largedesktop: {
+      breakpoint: { max: 3000, min: 1600 },
+      items: 4,
+    },
     desktop: {
-      breakpoint: { max: 3000, min: 1024 },
+      breakpoint: { max: 1600, min: 1200 },
       items: 3,
     },
     tablet: {
-      breakpoint: { max: 1024, min: 464 },
+      breakpoint: { max: 1200, min: 800 },
       items: 2,
     },
     mobile: {
-      breakpoint: { max: 464, min: 0 },
+      breakpoint: { max: 800, min: 0 },
       items: 1,
     },
   };
