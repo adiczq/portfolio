e765fa2f7d7278a5d22ccbc788067933c8f72eb7
Adrian Lacheta <adiczq@gmail.com>
2024-02-19 15:55:51 +0100

    Update Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import "./Navbar.css";
-import menu from "../../assets/menu.png";
 import { Link } from "react-scroll";
 
 const Navbar = () => {
@@ -64,12 +63,18 @@ const Navbar = () => {
           Contact
         </Link>
       </div>
-      <img
-        src={menu}
-        alt="Menu"
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        x="0px"
+        y="0px"
+        width="100"
+        height="100"
+        viewBox="0 0 72 72"
         className="mobMenu"
         onClick={() => setShowMenu(!showMenu)}
-      />
+      >
+        <path d="M56 48c2.209 0 4 1.791 4 4 0 2.209-1.791 4-4 4-1.202 0-38.798 0-40 0-2.209 0-4-1.791-4-4 0-2.209 1.791-4 4-4C17.202 48 54.798 48 56 48zM56 32c2.209 0 4 1.791 4 4 0 2.209-1.791 4-4 4-1.202 0-38.798 0-40 0-2.209 0-4-1.791-4-4 0-2.209 1.791-4 4-4C17.202 32 54.798 32 56 32zM56 16c2.209 0 4 1.791 4 4 0 2.209-1.791 4-4 4-1.202 0-38.798 0-40 0-2.209 0-4-1.791-4-4 0-2.209 1.791-4 4-4C17.202 16 54.798 16 56 16z"></path>
+      </svg>
       <div
         className="navMobMenu"
         style={{ display: showMenu ? "flex" : "none" }}
