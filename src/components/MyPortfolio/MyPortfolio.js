7c140118b49a78c58cce965c8a37338cc8cdc650
Adrian Lacheta <adiczq@gmail.com>
2024-02-19 13:33:22 +0100

    Create MyPortfolio.js

---
+++ src/components/MyPortfolio/MyPortfolio.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./MyPortfolio.css";
+
+const MyPortfolio = () => {
+  return (
+    <section id="myPortfolio">
+      <h2 className="myPortfolioTitle">My Portfolio</h2>
+      <span className="myPortfolioDesc">
+        Welcome to my portfolio of web applications! Throughout my journey in
+        programming and my time with GoIT course, I've had the pleasure of
+        crafting various web applications that showcase my skills and passion
+        for development. Each project in my portfolio is a testament to my
+        dedication to creating functional, user-friendly experiences. Leveraging
+        technologies such as HTML, CSS, JavaScript, and frameworks like React.js
+        and Node.js, I've brought innovative ideas to life in the digital realm.
+        Whether it's streamlining business processes, enhancing user engagement,
+        or simply bringing creative concepts to fruition, my portfolio reflects
+        the diverse range of projects I've tackled and the versatility I bring
+        to the table as a developer.
+      </span>
+    </section>
+  );
+};
+
+export default MyPortfolio;
