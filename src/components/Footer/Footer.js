0ba862aa5947d0915fabbe1e4daafb37a7915047
Adrian Lacheta <adiczq@gmail.com>
2024-02-20 11:58:25 +0100

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,11 +2,7 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
-  return (
-    <footer className="footer">
-      Copyright &#169; 2024 Adrian Lacheta. All right received.
-    </footer>
-  );
+  return <footer className="footer">Created by Adiczq </footer>;
 };
 
 export default Footer;
